3676bb5e2764106951c209dd88ff3814601ec71a
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2016-06-29 23:58:44 -0700

    Removed debugging cruft

--- src/controller.js
+++ src/controller.js
@@ -33,12 +33,10 @@ function loadSpellChecker() {
   // return new Typo('en_US', 'typo/dictionaries/en_US/en_US.aff', 'typo/dictionaries/en_US/en_US.dic');
   if (localStorage) {
     if (localStorage.affData && localStorage.wordsData) {
-      console.log("hit cache!");
       spellChecker = new Typo("en_US", localStorage.affData, localStorage.wordsData);
     } else {
       $.get('typo/dictionaries/en_US/en_US.aff', function (affData) {
         $.get('typo/dictionaries/en_US/en_US.dic', function (wordsData) {
-          console.log("missed cache!");
           localStorage.affData = affData;
           localStorage.wordsData = wordsData;
           spellChecker = new Typo("en_US", localStorage.affData, localStorage.wordsData);
